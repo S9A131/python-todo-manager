f1dc3bed744aaa30a541fa8924c0f5575497e689
S9A131 <susmitachoppa@gmail.com>
2026-01-06 13:22:50 +0530

    Add files via upload
    The Fibonacci series is a sequence of numbers in which each number is the sum of the two preceding numbers.
    The series typically starts with 0 and 1.

---
+++ Fibonacci.py
@@ -0,0 +1,12 @@
+# fibonacci.py
+
+def fibonacci(n):
+    a, b = 0, 1
+    series = []
+    for _ in range(n):
+        series.append(a)
+        a, b = b, a + b
+    return series
+
+num = int(input("Enter how many terms: "))
+print("Fibonacci series:", fibonacci(num))
